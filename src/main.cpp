43e985b4cf780e64f627d918177a220f9cc993bb
Shiwen An <sweynan@icloud.com>
2023-12-07 11:36:32 +0900

    CMake and main framework

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "svm_common.h" // Include LightSVM header
+#include "svm_learn.h" // Include LightSVM header
+#include "kernel.h" // Include LightSVM header
+
+
+int main() {
+
+    return 0;
+}
